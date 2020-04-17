20859172bda4761d359a7cd1f21f6538ec6c1437
Dai Nguyen <dainguyen@Dais-MacBook-Pro.local>
2020-04-16 17:07:40 -0700

    make songs random

---
+++ src/const.js
@@ -0,0 +1,22 @@
+export const SONGS = [
+  {
+    artist: 'Nujabes',
+    title: 'Luv(sic) Part 2',
+    blockQuote: {
+      author: 'Shing02',
+      quote: [
+        "The rhymes will heal 'cause I believe in music",
+        "In times of need I won't be leaving you sick",
+        "The beat plus the melody's the recipe",
+        'Your vibe surely brings out the best in me',
+      ],
+      cite: 'https://en.wikipedia.org/wiki/Luv(Sic)_Hexalogy#Luv(Sic)_Part_2',
+    },
+    youtubeID: 'If9HEbCpvB8',
+  },
+  {
+    artist: 'DJ Okawari',
+    title: 'The Bonds',
+    youtubeID: '2WNaBNvxNJo',
+  },
+];
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,36 +2,36 @@ import React from 'react';
 import styled from 'styled-components';
 
 import SEO from '../components/seo';
+import { SONGS } from '../const';
 import './style.css';
 import 'typeface-comfortaa';
 
-const IndexPage = () => (
-  <Container>
-    <SEO />
-    <Blockquote cite="https://en.wikipedia.org/wiki/Luv(Sic)_Hexalogy#Luv(Sic)_Part_2">
-      <p>
-        The rhymes will heal 'cause I believe in music
-        <br />
-        In times of need I won't be leaving you sick
-        <br />
-        The beat plus the melody's the recipe
-        <br />
-        Your vibe surely brings out the best in me
-      </p>
-      <footer>
-        —Shing02, <cite>Luv(Sic) Part 2</cite>
-      </footer>
-    </Blockquote>
-    <IFrame
-      width="560"
-      height="315"
-      src="https://www.youtube.com/embed/If9HEbCpvB8"
-      frameBorder="0"
-      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-    ></IFrame>
-  </Container>
-);
+const IndexPage = () => {
+  const song = SONGS[Math.floor(Math.random() * SONGS.length)];
+  const { title, blockQuote, youtubeID } = song;
+
+  return (
+    <Container>
+      <SEO />
+      {blockQuote && (
+        <Blockquote cite={blockQuote.cite}>
+          <Quote>{blockQuote.quote.join('\n')}</Quote>
+          <footer>
+            —{blockQuote.author}, <cite>{title}</cite>
+          </footer>
+        </Blockquote>
+      )}
+      <IFrame
+        width="560"
+        height="315"
+        src={`https://www.youtube.com/embed/${youtubeID}`}
+        frameBorder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></IFrame>
+    </Container>
+  );
+};
 
 const Container = styled.div`
   align-items: center;
@@ -50,6 +50,10 @@ const Blockquote = styled.blockquote`
     margin: 0;
     font-size: 0.875rem;
   }
+`;
+
+const Quote = styled.p`
+  white-space: pre-wrap;
 `;
 
 const IFrame = styled.iframe`
