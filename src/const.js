5085b98815284cc7bced976ff76b8ac92991d2a1
Dai Nguyen <dainguyen@Dais-MacBook-Pro.local>
2020-06-18 19:09:08 -0700

    add song

--- src/const.js
+++ src/const.js
@@ -2,6 +2,7 @@ export const SONGS = [
   {
     artist: 'Nujabes',
     title: 'Luv(sic) Part 2',
+    youtubeID: 'If9HEbCpvB8',
     blockQuote: {
       author: 'Shing02',
       quote: [
@@ -10,13 +11,25 @@ export const SONGS = [
         "The beat plus the melody's the recipe",
         'Your vibe surely brings out the best in me',
       ],
-      cite: 'https://en.wikipedia.org/wiki/Luv(Sic)_Hexalogy#Luv(Sic)_Part_2',
+      cite: 'https://en.wikipedia.org/wiki/Shing02',
     },
-    youtubeID: 'If9HEbCpvB8',
   },
   {
     artist: 'DJ Okawari',
     title: 'The Bonds',
     youtubeID: '2WNaBNvxNJo',
+  },
+  {
+    artist: 'DJ Okawari',
+    title: 'Midnight Train',
+    youtubeID: 'W72LiPMNs9E',
+    blockQuote: {
+      author: 'Emi Meyer',
+      quote: [
+        'When the streets should be black',
+        'But Kowloon is a sleepless neon nonstop hangover',
+      ],
+      cite: 'https://en.wikipedia.org/wiki/Emi_Meyer',
+    },
   },
 ];
