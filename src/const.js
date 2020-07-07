271b1f29ddef29164d5f3850b15dd426209eed8d
Dai Nguyen <dainguyen@Dais-MacBook-Pro.local>
2020-07-07 00:08:33 -0700

    add new songs

--- src/const.js
+++ src/const.js
@@ -1,5 +1,35 @@
 export const SONGS = [
   {
+    artist: 'Utada',
+    title: 'Kremlin Dusk',
+    youtubeID: '0e_CCrcSC_E',
+    blockQuote: {
+      author: 'Utada Hikaru',
+      quote: ['All along', 'I was searching for', 'my Lenore'],
+      cite: 'https://en.wikipedia.org/wiki/Utada_Hikaru',
+    },
+  },
+  {
+    artist: 'Blazo',
+    title: 'Alone Journey',
+    youtubeID: '3i6VLj4JE24',
+  },
+  {
+    artist: 'Nujabes',
+    title: 'Luv(sic) Part 4',
+    youtubeID: 'a1GDdawym1U',
+    blockQuote: {
+      author: 'Shing02',
+      quote: ["When you took my hand, I couldn't hide it"],
+      cite: 'https://en.wikipedia.org/wiki/Shing02',
+    },
+  },
+  {
+    artist: 'Parov Stelar',
+    title: 'Nowhere',
+    youtubeID: '--kgXEbP8Tg',
+  },
+  {
     artist: 'Nujabes',
     title: 'Luv(sic) Part 2',
     youtubeID: 'If9HEbCpvB8',
@@ -15,9 +45,24 @@ export const SONGS = [
     },
   },
   {
+    artist: 're:plus',
+    title: 'A Fall and Rebirth',
+    youtubeID: 'lyy55XMcZSY',
+  },
+  {
     artist: 'DJ Okawari',
     title: 'The Bonds',
     youtubeID: '2WNaBNvxNJo',
+  },
+  {
+    artist: 'Nujabes',
+    title: 'Eclipse',
+    youtubeID: 'jd_CQeym-6U',
+    blockQuote: {
+      author: 'Substantial',
+      quote: ["This is for the things I don't say enough"],
+      cite: 'https://en.wikipedia.org/wiki/Substantial_(rapper)',
+    },
   },
   {
     artist: 'DJ Okawari',
@@ -25,11 +70,39 @@ export const SONGS = [
     youtubeID: 'W72LiPMNs9E',
     blockQuote: {
       author: 'Emi Meyer',
-      quote: [
-        'When the streets should be black',
-        'But Kowloon is a sleepless neon nonstop hangover',
-      ],
+      quote: ["Timing has never been my strength, ain't it strange?"],
       cite: 'https://en.wikipedia.org/wiki/Emi_Meyer',
     },
+  },
+  {
+    artist: 'Elliott Smith',
+    title: 'Oh Well, OK',
+    youtubeID: '5qhPPnroLQ4',
+    blockQuote: {
+      author: 'Elliott Smith',
+      quote: ["Here's the silhouette of face always turned away"],
+      cite: 'https://en.wikipedia.org/wiki/Elliott_Smith',
+    },
+  },
+  {
+    artist: 'Elliott Smith',
+    title: "I Didn't Understand",
+    youtubeID: 'lFIkik2I_Pc',
+    blockQuote: {
+      author: 'Elliott Smith',
+      quote: ['You once talked to me about love'],
+      cite: 'https://en.wikipedia.org/wiki/Elliott_Smith',
+    },
+  },
+  {
+    artist: 'Kara Comparetto',
+    composer: 'Yasunori Mitsuda',
+    title: 'At the Bottom of Night',
+    youtubeID: 'uCjzlwNB75U',
+  },
+  {
+    artist: 'IU',
+    title: '밤편지',
+    youtubeID: 'BzYnNdJhZQw',
   },
 ];
